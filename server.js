3e07c41cfaacc7ac354d8e6c350f75fcbfbd1154
Pawel Domagala <domagalapawel11@gmail.com>
2023-08-02 00:06:57 +0200

    Add passport

--- server.js
+++ server.js
@@ -2,16 +2,41 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const hbs = require('express-handlebars');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const session = require('express-session');
 
 const app = express();
 
-app.engine('hbs', hbs({ extname: 'hbs', layoutsDir: './layouts', defaultLayout: 'main' }));
+// configure passport provider options
+passport.use(new GoogleStrategy({
+  clientID: '295825791215-4vccp2cst0s52cv51fvqa86p7beprjpa.apps.googleusercontent.com',
+  clientSecret: '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',
+  callbackURL: 'http://localhost:8000/auth/google/callback'
+}, (accessToken, refreshToken, profile, done) => {
+done(null, profile);
+}));
+
+// serialize user when saving to session
+passport.serializeUser((user, serialize) => {
+serialize(null, user);
+});
+
+// deserialize user when reading from session
+passport.deserializeUser((obj, deserialize) => {
+deserialize(null, obj);
+});
+
+app.engine('hbs', hbs({extname: 'hbs', layoutsDir: './layouts', defaultLayout: 'main'}));
 app.set('view engine', '.hbs');
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, '/public')));
+app.use(session({secret: 'anything'}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get('/', (req, res) => {
   res.render('index');
